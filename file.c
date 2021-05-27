d808ad146dc757277558915d28ba518179ad7a46
Matty Wong <wonghzm@stanford.edu>
2021-05-26 17:32:47 -0700

    Hopefully completed basic file and inode capability

--- file.c
+++ file.c
@@ -6,8 +6,26 @@
 #include "diskimg.h"
 
 int file_getblock(struct unixfilesystem *fs, int inumber, int blockNum, void *buf) {
-    // Remove the following placeholder code and add your implementation
-    fprintf(stderr, "file_getblock(inumber = %d, blockNum = %d) "
-            "unimplemented. returning -1\n", inumber, blockNum);
-    return -1;
+    struct inode inp;
+	if (inode_iget(fs, inumber, &inp)) {
+		fprintf(stderr, "Couldn't fetch inode with number: %d\n", inumber);
+		return -1;
+	}
+	int sector_from_inode = inode_indexlookup(fs, &inp, blockNum);
+	if (sector_from_inode == -1) {
+		fprintf(stderr, "Couldn't fetch sector number of inode: %d, block number: %d\n", inumber, blockNum);
+		return -1;
+	}
+	
+	int fd = fs->dfd;
+	int read;
+	if ((read = diskimg_readsector(fd, sector_from_inode, buf)) != DISKIMG_SECTOR_SIZE) {
+		fprintf(stderr, "Invalid read of sector: %d. Only read %d bytes.\n", sector_from_inode, read);
+		return -1;
+	}
+	
+	int inode_size = inode_getsize(&inp);
+	if (blockNum == inode_size/DISKIMG_SECTOR_SIZE) return inode_size % DISKIMG_SECTOR_SIZE; 
+	
+    return DISKIMG_SECTOR_SIZE;
 }
--- inode.c
+++ inode.c
@@ -4,21 +4,74 @@
 #include "inode.h"
 #include "diskimg.h"
 #include <stdbool.h>
+#include <string.h>
 
 #define INODES_PER_BLOCK (DISKIMG_SECTOR_SIZE/sizeof(struct inode))
 #define NUM_BLOCK_NUMS_PER_BLOCK (DISKIMG_SECTOR_SIZE/sizeof(uint16_t))
 
 int inode_iget(struct unixfilesystem *fs, int inumber, struct inode *inp) {
-    // Remove the placeholder code below and add your implementation.
-    fprintf(stderr, "inode_get(inumber=%d) not implemented, returning -1\n",
-            inumber);
-    return -1;
+	int fd = fs->dfd;
+	int num_iblocks = fs->superblock.s_isize;
+	int inode_block = (inumber-ROOT_INUMBER) / INODES_PER_BLOCK;
+	if (inode_block < 0 || inode_block >= num_iblocks) {
+		fprintf(stderr, "Invalid inode_block: %d specified\n", inode_block);
+		return -1;
+	}
+	struct inode buf[INODES_PER_BLOCK];
+	int read;
+	if ((read = diskimg_readsector(fd, INODE_START_SECTOR + inode_block, buf)) != DISKIMG_SECTOR_SIZE) {
+		fprintf(stderr, "Invalid read of inode block: %d. Only read %d bytes.\n", inode_block, read);
+		return -1;
+	}
+	
+	memcpy(inp, buf + (inumber-ROOT_INUMBER)%INODES_PER_BLOCK, sizeof(struct inode));
+    return 0;
 }
 
 int inode_indexlookup(struct unixfilesystem *fs, struct inode *inp, int blockNum) {
-    // Remove the placeholder code below and add your implementation.
-    fprintf(stderr, "inode_indexlookup(blockNum=%d) not implemented, "
-            "returning -1\n", blockNum);
+	if (blockNum < 0) {
+		fprintf(stderr, "Negative block num given: %d\n", blockNum);
+		return -1;
+	}
+	
+	int fd = fs->dfd;
+	int read;
+	
+    if (inp->i_mode & ILARG) {
+		int i_addr_idx = blockNum / NUM_BLOCK_NUMS_PER_BLOCK;
+		if (i_addr_idx >= 7) {
+			blockNum -= NUM_BLOCK_NUMS_PER_BLOCK*7;
+			int first_idx = blockNum / NUM_BLOCK_NUMS_PER_BLOCK;
+			if (first_idx < 0 || first_idx >= (int)NUM_BLOCK_NUMS_PER_BLOCK) {
+				fprintf(stderr, "Invalid first index: %d\n", first_idx);
+				return -1;
+			}
+			uint16_t buf[NUM_BLOCK_NUMS_PER_BLOCK];
+		
+			if ((read = diskimg_readsector(fd, inp->i_addr[7], buf)) != DISKIMG_SECTOR_SIZE) {
+				fprintf(stderr, "Invalid read of sector: %d. Only read %d bytes.\n", inp->i_addr[i_addr_idx], read);
+				return -1;
+			}
+			if ((read = diskimg_readsector(fd, buf[first_idx], buf)) != DISKIMG_SECTOR_SIZE) {
+				fprintf(stderr, "Invalid read of sector: %d. Only read %d bytes.\n", inp->i_addr[i_addr_idx], read);
+				return -1;
+			}
+			
+			return buf[blockNum % NUM_BLOCK_NUMS_PER_BLOCK];
+		}
+		
+		uint16_t buf[NUM_BLOCK_NUMS_PER_BLOCK];
+		if ((read = diskimg_readsector(fd, inp->i_addr[i_addr_idx], buf)) != DISKIMG_SECTOR_SIZE) {
+			fprintf(stderr, "Invalid read of sector: %d. Only read %d bytes.\n", inp->i_addr[i_addr_idx], read);
+			return -1;
+		}
+		return buf[blockNum % NUM_BLOCK_NUMS_PER_BLOCK];
+		
+	} else {
+		if (blockNum < 8) return inp->i_addr[blockNum];
+		fprintf(stderr, "Small inode addressing but block num is: %d\n", blockNum);
+	}
+	
     return -1;
 }
 
